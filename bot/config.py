e5f42070be8e6b913ef1531784b2714fb08c4452
HaradaYuta <82194235+harada-yuta@users.noreply.github.com>
2021-05-08 13:35:10 +0900

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -34,15 +34,15 @@ auth4.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api4 = tweepy.API(auth4)
 
 # Bot5
-ACCESS_TOKEN = "1386176723155251200-bdzc1IhYL4Hy51ubw06qMMDUQkZWBp"
-ACCESS_SECRET = "BxxIrMmLXhVLjbR6qYW48mN9CBhefZrLy4Bnu0E5Amc3a"
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
 auth5 = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth5.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api5 = tweepy.API(auth5)
 
 # Bot6
-ACCESS_TOKEN = "1386177079486550018-hn9nR9MvdtM4tHrXWMH5kRsrdlv2of"
-ACCESS_SECRET = "GmuFIfaYUcRGn3a4lTRmIFbPuX0lBMk7vZpLEcLbijniX"
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
 auth6 = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth6.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api6 = tweepy.API(auth6)
